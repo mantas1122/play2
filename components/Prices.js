db6e49dd58bd8876300faf3901f1fcec10c80b56
mantas1122 <mantas.stanionis@gmail.com>
2019-11-17 23:39:38 +0200

    1

--- components/Prices.js
+++ components/Prices.js
@@ -2,19 +2,27 @@ import { Card, CardTitle, CardBody, CardText, CardImg } from "reactstrap";
 
 class Prices extends React.Component {
   render() {
-    return (
-      <div>
-      <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>{this.props.bpi.kind}</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-    </div>
-    );
+    var cardList = this.props.bpi.items.map(function(item) {
+      return (
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="/assets/318x180.svg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>{item.snippet.title}</CardTitle>
+            <CardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </CardText>
+          </CardBody>
+        </Card>
+      );
+    });
+
+    return <div>{cardList}</div>;
   }
 }
 
