483762a380668292aa605fc1aa69047f5d9601f0
mantas1122 <mantas.stanionis@gmail.com>
2019-11-17 21:52:34 +0200

    2

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,35 @@
-import Fetch from 'isomorphic-unfetch';
-import Layout from '../components/Layout';
+import Fetch from "isomorphic-unfetch";
+import Layout from "../components/Layout";
 
-import Prices from '../components/Prices';
+import Prices from "../components/Prices";
 
-const Index = (props) => (
+const Index = props => (
   <Layout>
     <div>
       <h1>Welcome to BitzPrice ddd</h1>
       <p>Check current Bitcoin rate</p>
-      <Prices bpi={props.bpi}/>
+      <Prices bpi={props.bpi} />
     </div>
   </Layout>
 );
 
 Index.getInitialProps = async function() {
-  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  // const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const dkey = "AIzaSyCgYKXEBHLkcp5K3ep2Ue9G4GJHWerThVQ";
+  const channelId = "UCCBVCTuk6uJrN3iFV_3vurg";
+  const res = await fetch(
+    "https://www.googleapis.com/youtube/v3/search?" +
+      "key=" +
+      dkey +
+      "&channelId=" +
+      channelId +
+      "&part=snippet,id&order=date&maxResults=20"
+  );
   const data = await res.json();
 
   return {
-    bpi: data.bpi
+    bpi: data.kind
   };
-}
+};
 
 export default Index;
